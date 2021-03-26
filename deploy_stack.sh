10affcbf04ace3c10dd2b2576090581ffff82087
peglegpete <luis.oala@hhi.fraunhofer.de>
2021-03-26 22:47:50 +0100

    uncomment deploy

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#python3 -m venv .venv
-#source .venv/bin/activate
-#pip3 install -r requirements.txt
-#npm install -g aws-cdk@1.65.0
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+npm install -g aws-cdk@1.65.0
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account | tr -d '"')
 AWS_REGION=$(aws configure get region)
